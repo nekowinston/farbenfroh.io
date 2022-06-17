a510bda4d36b36e24f5660be32d8d50a80224d13
Ace Daksh <ektadaksh7@gmail.com>
2022-06-16 17:59:55 -0700

    Hello world!

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use iced::{
+    alignment, button, scrollable, slider, text_input, Button, Checkbox, Color,
+    Column, Container, ContentFit, Element, Length, Radio, Row, Sandbox,
+    Scrollable, Settings, Slider, Space, Text, TextInput, Toggler,
+};
+
+
+pub fn main() -> iced::Result {
+    Hello::run(Settings::default())
+}
+struct Hello;
+
+impl Sandbox for Hello {
+    type Message = ();
+    
+    fn new() -> Hello {
+        Hello
+    }
+
+    fn title(&self) -> String {
+        String::from("Farbenfroh")
+    }
+    
+    fn update(&mut self, message: Self::Message) {
+    }
+
+    fn view(&mut self) -> Element<Self::Message> {
+        Text::new("Hello, world!").into()
+    }
 }
