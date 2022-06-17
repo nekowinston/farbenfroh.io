afee9fbee2942be802ab41ee9c49567c3c5d55c7
Ace Daksh <ektadaksh7@gmail.com>
2022-06-17 08:56:02 -0700

    First UI Implementation

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 use iced::{
-    alignment, button, scrollable, slider, text_input, Button, Checkbox, Color,
+    alignment, button, scrollable, slider, text_input, Alignment, Button, Checkbox, Color,
     Column, Container, ContentFit, Element, Length, Radio, Row, Sandbox,
     Scrollable, Settings, Slider, Space, Text, TextInput, Toggler,
 };
@@ -8,13 +8,22 @@ use iced::{
 pub fn main() -> iced::Result {
     Hello::run(Settings::default())
 }
-struct Hello;
+
+#[derive(Default)]
+struct Hello {
+    upload: button::State,
+}
+
+#[derive(Debug, Clone, Copy)]
+enum Message {
+        ButtonPressed,
+}
 
 impl Sandbox for Hello {
-    type Message = ();
+    type Message = Message;
     
-    fn new() -> Hello {
-        Hello
+    fn new() -> Self {
+        Self::default()
     }
 
     fn title(&self) -> String {
@@ -22,9 +31,21 @@ impl Sandbox for Hello {
     }
     
     fn update(&mut self, message: Self::Message) {
+        
     }
 
     fn view(&mut self) -> Element<Self::Message> {
-        Text::new("Hello, world!").into()
+        Column::new()
+            .padding(20)
+            .align_items(Alignment::Center)
+            .push(
+                Text::new("faerber!")
+                    .size(100)
+            )
+            .push(  
+                Button::new(&mut self.upload, Text::new("Upload"))
+                    .on_press(Message::ButtonPressed),
+            )
+            .into()
     }
 }
