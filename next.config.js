8eab6594dea5d5a995e0d0eafaf6387e4d06376b
winston <hey@winston.sh>
2023-03-11 18:11:32 +0100

    chore: update rust to v1.70.0-nightly

--- next.config.js
+++ next.config.js
@@ -33,8 +33,18 @@ module.exports = {
       },
     ]
   },
-  webpack(config) {
-    config.experiments = { asyncWebAssembly: true }
+  webpack(config, { isServer, dev }) {
+    // Use the client static directory in the server bundle and prod mode
+    // Fixes `Error occurred prerendering page "/"`
+    config.output.webassemblyModuleFilename =
+      isServer && !dev
+        ? '../static/wasm/[modulehash].wasm'
+        : 'static/wasm/[modulehash].wasm'
+
+    // Since Webpack 5 doesn't enable WebAssembly by default, we should do it manually
+    config.experiments = { ...config.experiments, asyncWebAssembly: true }
+
+    // Add SVG import support
     config.module.rules.push({
       test: /\.svg$/i,
       issuer: /\.[jt]sx?$/,
